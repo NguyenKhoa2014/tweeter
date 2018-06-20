38cc61f9e048d345ea7bbb700b5526eb08dafa3c
Khoa Nguyen <khoanguyen@Khoas-MacBook-Pro-2.local>
2018-06-19 17:46:27 -0700

    add tweet section

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,29 +5,20 @@ $(document).ready(function() {
  
     console.log($("#tweet-textarea").val(), $("#tweet-textarea").val().length);
     let aString = $("#tweet-textarea").val();
-    //let charCount = countingCharacters(aString);
-    //$("#charCount").val() = charCount;
-    //console.log(charCount);
+ 
   });
   $("#tweet-textarea").on('input propertychange paste', function(){
-    //let aString = $("#tweet-textarea").val();
-    //const MAX = 140;
-    //let availChar = MAX - aString.length;
-    //$("#tweet-textarea").html(availChar);
+
     const $counter = $(".counter");
-    //var myCounter = document.getElementsByClassName('.counter')
     const tweetContent = $(this).val();
     const charCount = Array.from(tweetContent).length;
     const charsRemaining = 140 - charCount;
     $counter.html(charsRemaining);
-    
     var el = $counter[0];
     if(charsRemaining < 0){
       console.log("no more characters");
       el.style.color = "#FF0000";
-      el.classList.add('.negative')
-       
-       
+      el.classList.add('.negative');
     }
     else{
       console.log("still characters");
@@ -35,11 +26,22 @@ $(document).ready(function() {
       el.classList.remove('.negative')
     }
   })
+  $("article").bind("mouseover", function(){
+    var element = $("article");
+    //console.log(element);
+    //console.log(element.classList);
+    //element.style.filter += 'contrast(110%)';
+    element.addClass('.tweet-mouseover');
+    
+    //element.classList.add('.tweet-mouseover');
+  });
 
-});
+  $("article").bind("mouseout", function(){
+    var element = $("article");
+    //console.log(element);
+    //element.classList.add('.tweet-mouseout');
+  });
 
-function countingCharacters(input){
-  const MAX = 140;
-  let inputLength = input.length;
-  return MAX - inputLength;
-} 
+}); //end of document ready
+
+ 
